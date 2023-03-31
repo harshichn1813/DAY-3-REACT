141e1d257ba4e0631a8df7feb08b3064647bd25f
Harshitha <108514040+harshichn1813@users.noreply.github.com>
2023-03-31 12:52:29 +0530

    Add files via upload

---
+++ src/Components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../Components/Contact.css"
+
+export default function Contact() {
+  return (
+    <section id='Contactj'>
+       <div className='Contact'>
+           <h1>Contact</h1>
+       </div>
+    </section>
+  )
+}
